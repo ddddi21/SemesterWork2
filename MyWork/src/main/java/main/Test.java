1a59db9ac85d682025915339d40fe7a49f7d71e8
Dilyara <dilyra.as@gmail.com>
2020-12-21 01:36:47 +0300

    отображение рисунка у угадывающих (ура!)

--- MyWork/src/main/java/main/Test.java
+++ MyWork/src/main/java/main/Test.java
@@ -1,6 +1,5 @@
 package main;
 
-import drawing.Draw;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -9,10 +8,9 @@ import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ColorPicker;
-import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import drawing.Draw;
 import network.Connection;
 import network.ConnectionListener;
 import services.SendDrawingService;
@@ -21,18 +19,22 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 
-public class Client extends Application implements ConnectionListener {
-    public Connection connection;
-    public Draw draw = new Draw();
-    SendDrawingService drawingService = new SendDrawingService();
+public class Test extends Application implements ConnectionListener {
     private final ArrayList<Connection> connectionArrayList = new ArrayList<>();
+    Draw draw = new Draw();
+    /*SendDrawingService drawingService = new SendDrawingService();*/  //Нам ето не нужон
+    private Connection connection;
 
-
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) {
         try {
             connection = new Connection(this, "localhost", 8181);
+            System.out.println("Test connection: "+ connection);
+            System.out.println("Test socket: "+ connection.socket);
 //            txtAreaDisplay.appendText("Connected. \n");
         } catch (IOException e) {
 //            printMessage("Connection exception: " + e);
@@ -55,9 +57,10 @@ public class Client extends Application implements ConnectionListener {
                 try {
                     Stage stage = new Stage();
                     draw.start(stage);
-                    if(connection.isCommander)
-                    printMessage("you are drawing!");
-                    ((Node)(event.getSource())).getScene().getWindow().hide();
+                    if (connection.isCommander)
+                        printMessage("you are drawing!");
+                    else printMessage("ты угадываешь!");
+                    ((Node) (event.getSource())).getScene().getWindow().hide();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -76,13 +79,8 @@ public class Client extends Application implements ConnectionListener {
         });
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-
-@Override
-public void onConnectionReady(Connection connection) {
+    @Override
+    public void onConnectionReady(Connection connection) {
 //    connectionArrayList.add(connection);
 //    while(true){
 //        if (connectionArrayList.get(0) == connection) {
@@ -91,29 +89,33 @@ public void onConnectionReady(Connection connection) {
 //        }
 //    }
 //    printMessage("Connection ready..");
-}
+    }
 
     @Override
     public void onReceiveString(Connection connection, String value) {
-        if(!value.equals("null")) {
+        System.out.println("Test/Recive: "+value);
+        if (!value.equals("null")) {
             if (value.equals("StartFirst")) {
                 connection.isCommander = true;
-            } else {
-                //принимаю что началась рисовка
-                if (value.equals("GameIsStarting")) {
-                    onStartDrawing(connection, true);
-                } else{
-                    //проверяю на угаданное слово
-                    if(value.equals("win")){
-                        printMessage("Вы победили!");
-                        theEnd();
-                    }
-                        else {
-                        connection.guessWord = draw.guessWord;
-                            printMessage(value);
-                    }
+                System.out.println("Найден командер");
+            } else if (value.equals("GameIsStarting")) {
+                onStartDrawing(connection, true);
+                System.out.println("Начало игры");
+            } else if((value.charAt(0)+"").equals("#")){
+                if((value.charAt(1)+"").equals("s")){
+                    String info = value.substring(2);
+                    System.out.println("Test/Получил: "+info);
+                    if(!connection.isCommander) draw.startDraw(draw.graphicsContext,info);
+                }else if ((value.charAt(1)+"").equals("m")) {
+                    String info = value.substring(2);
+                    System.out.println("Test/Получил: "+info);
+                    if(!connection.isCommander) draw.draw(draw.graphicsContext,info);
                 }
-            }
+                else if((value.charAt(1)+"").equals("c")){
+                    if(!connection.isCommander) draw.clear();
+                }
+                else printMessage(value);
+            }else printMessage(value);
         }
     }
 
@@ -134,15 +136,10 @@ public void onConnectionReady(Connection connection) {
         draw.isStart = true;
     }
 
-    private synchronized void printMessage(String message){
+    private synchronized void printMessage(String message) {
         Platform.runLater(() -> {
             Draw.txtAreaDisplay.appendText(message + "\n");
         });
-    }
-
-    //возможно в будущем добавлю вывод имени угадавшего
-    private void theEnd(){
-
     }
 
 }
